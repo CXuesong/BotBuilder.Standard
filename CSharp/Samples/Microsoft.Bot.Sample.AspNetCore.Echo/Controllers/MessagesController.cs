538d2d32e3e8e1d335f10d514f4a94a13f3edaf5
forest93 <forest93@163.com>
2017-03-11 20:49:53 +0800

    Copy example code of EchoBot from .NET Framework version.

--- CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.Echo/Controllers/MessagesController.cs
+++ CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.Echo/Controllers/MessagesController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Connector;
+using Microsoft.Bot.Sample.EchoBot;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 
 namespace Microsoft.Bot.Sample.AspNetCore.Echo.Controllers
 {
@@ -23,19 +28,50 @@ namespace Microsoft.Bot.Sample.AspNetCore.Echo.Controllers
         [HttpPost]
         public virtual async Task<OkResult> Post([FromBody]Activity activity)
         {
-            var appCredentials = new MicrosoftAppCredentials(this.configuration);
-            var client = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
-            var reply = activity.CreateReply();
-            if (activity.Type == ActivityTypes.Message)
+            if (activity == null) goto FINAL;
+            // one of these will have an interface and process it
+            switch (activity.GetActivityType())
             {
-                reply.Text = $"echo: {activity.Text}";
+                case ActivityTypes.Message:
+                    //await Conversation.SendAsync(activity, () => new EchoDialog());
+                    //await Conversation.SendAsync(activity, () => EchoCommandDialog.dialog);
+                    //await Conversation.SendAsync(activity, () => new EchoAttachmentDialog());
+                    await Conversation.SendAsync(activity, () => EchoChainDialog.dialog);
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    IConversationUpdateActivity update = activity;
+                    using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, activity))
+                    {
+                        var client = scope.Resolve<IConnectorClient>();
+                        if (update.MembersAdded.Any())
+                        {
+                            var reply = activity.CreateReply();
+                            foreach (var newMember in update.MembersAdded)
+                            {
+                                if (newMember.Id != activity.Recipient.Id)
+                                {
+                                    reply.Text = $"Welcome {newMember.Name}!";
+                                }
+                                else
+                                {
+                                    reply.Text = $"Welcome {activity.From.Name}";
+                                }
+                                await client.Conversations.ReplyToActivityAsync(reply);
+                            }
+                        }
+                    }
+                    break;
+                case ActivityTypes.ContactRelationUpdate:
+                case ActivityTypes.Typing:
+                case ActivityTypes.DeleteUserData:
+                case ActivityTypes.Ping:
+                default:
+                    Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
+                    break;
             }
-            else
-            {
-                reply.Text = $"activity type: {activity.Type}";
-            }
-            await client.Conversations.ReplyToActivityAsync(reply);
-            return Ok();
+            FINAL:
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
         }
     }
 }

52100d68154ebf7e2ea4a8b52dc85701328d8e14
forest93 <forest93@163.com>
2017-03-15 03:45:13 +0800

    Add Microsoft.Bot.Sample.AspNetCore.SimpleEchoBot .

---
+++ CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.SimpleEchoBot/Controllers/MessagesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Sample.AspNetCore.SimpleEchoBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        private readonly IConfigurationRoot configuration;
+        private readonly MicrosoftAppCredentials credentials;
+
+        public MessagesController(IConfigurationRoot configuration, MicrosoftAppCredentials credentials)
+        {
+            this.configuration = configuration;
+            this.credentials = credentials;
+        }
+
+        [Authorize(Roles = "Bot")]
+        // POST api/values
+        [HttpPost]
+        public virtual async Task<OkResult> Post([FromBody]Activity activity)
+        {
+            var client = new ConnectorClient(new Uri(activity.ServiceUrl), credentials);
+            var reply = activity.CreateReply();
+            if (activity.Type == ActivityTypes.Message)
+            {
+                reply.Text = $"echo: {activity.Text}";
+            }
+            else
+            {
+                reply.Text = $"activity type: {activity.Type}";
+            }
+            await client.Conversations.ReplyToActivityAsync(reply);
+            return Ok();
+        }
+    }
+}
---
+++ CSharp/Samples/Microsoft.Bot.Sample.AspNetCore.SimpleEchoBot/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Bot.Connector;
+
+namespace Microsoft.Bot.Sample.AspNetCore.SimpleEchoBot
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_ => Configuration);
+            // Authentication for Microsoft Bot Framework.
+            services.AddSingleton(_ => new MicrosoftAppCredentials(Configuration,
+                _.GetService<ILoggerFactory>().CreateLogger<MicrosoftAppCredentials>()));
+
+            // Add framework services.
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new TrustServiceUrlAttribute());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseStaticFiles();
+
+            app.UseBotAuthentication(new StaticCredentialProvider(
+                Configuration.GetSection(MicrosoftAppCredentials.MicrosoftAppIdKey)?.Value,
+                Configuration.GetSection(MicrosoftAppCredentials.MicrosoftAppPasswordKey)?.Value));
+
+            app.UseMvc();
+        }
+    }
+}
